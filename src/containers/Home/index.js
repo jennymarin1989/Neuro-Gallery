5d495d44589dea82754d38dc84fba09ad9fb8d9b
jennymarin1989 <jenny.arenas@bbc.co.uk>
2019-10-18 15:00:05 +0100

    Add a link in Home component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../logo.svg'
 import Heading from '../../components/atoms/Heading'
 import './_home.scss';
 
@@ -11,16 +12,14 @@ const Home = () => {
         <img src={logo} className="home-logo" alt="logo" />
         <div id="cursor"> </div>
         <Heading heading="Welcome to Mind Gallery" tag="h1" className="home-title" />
-        <a
+        <Link
           className="home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          to="/instructions"
         >
           Start!
-        </a>
+        </Link>
       </header>
-    </div>
+    </div >
   );
 }
 
