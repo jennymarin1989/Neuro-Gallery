77d24af1b44d8aa4ab2c4b4348b3a541fd2e7eeb
jennymarin1989 <jenny.arenas@bbc.co.uk>
2019-10-18 14:58:58 +0100

    Add link in Intructions component and change link styling

--- src/containers/Instructions/index.js
+++ src/containers/Instructions/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Heading from '../../components/atoms/Heading'
 import TextArea from '../../components/atoms/TextArea'
 import './_instructions.scss';
@@ -6,13 +7,19 @@ import './_instructions.scss';
 
 const Instructions = () => {
 
-    return (
-        <div className="instructions">
-					<div id="cursor"> </div>
-          <Heading heading='Instructions' tag='h1' className="heading-instructions"/>
-          <TextArea className="text-area"/>
-        </div>
-    )
+  return (
+    <div className="instructions">
+      <div id="cursor"> </div>
+      <Heading heading='Instructions' tag='h1' className="heading-instructions" />
+      <TextArea className="text-area" />
+      <Link
+        className="blink-strength-link"
+        to="/blink"
+      >
+        Next
+      </Link>
+    </div>
+  )
 
 }
 
